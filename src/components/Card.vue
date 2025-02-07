27dfff8338d936f19e8f002b09808b84fc994614
pynhaXD <roma151205@mail.ru>
2025-02-07 22:27:38 +0700

    Card create

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,26 +1,50 @@
 <script>
-  export default {
-    name: "Card",
-    data() {
-      return {
-
-      };
+export default {
+  name: "Card",
+  props: {
+    title: String,
+    notes: Array,
+  },
+  computed: {
+    completedCount() {
+      return this.notes.filter(note => note.done).length;
     },
-    props: {
-      notes: {
-
-      },
+    isCompleted() {
+      return this.completedCount === this.notes.length;
     },
-    methods: {
-
+    isInProgress() {
+      return this.completedCount / this.notes.length >= 0.5;
+    }
+  },
+  watch: {
+    isCompleted(newVal) {
+      if (newVal) {
+        this.$emit("move-to-done");
+      }
     },
+    isInProgress(newVal, oldVal) {
+      if (newVal && !oldVal) {
+        this.$emit("move-to-inProgress");
+      } else if (!newVal && oldVal) {
+        this.$emit("move-back-to-todo");
+      }
+    }
   }
+};
 </script>
 
 <template>
-  <div></div>
+  <div class="card">
+    <h3>{{ title }}</h3>
+    <ul>
+      <li v-for="(task, index) in notes" :key="index">
+        <input type="checkbox" v-model="task.done" />
+        {{ task.text }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
+<style>
 
 </style>
